aae043531d52b6c26293f5517565a7bcf6e16572
cfg7am <cfg7am@gmail.com>
2025-08-10 15:48:13 +0900

    CORS 설정 수정.

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const corsOptions = {
 	origin: [
 		"https://vavava-file.vavava.app",
 		"https://vavava.app",
-		"http://localhost:32705",
+		"http://localhost:32750",
 		"http://172.17.0.1",
 	],
 	methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
